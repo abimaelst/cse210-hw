416fceeb44dde23efa0ff7267e029ab175662bc9
abimael.tavares <abimael.tavares@devsquad.com>
2024-01-13 07:44:03 -0300

    finished prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter the score: ");
+        int grade = int.Parse(Console.ReadLine());
+
+        string letter;
+        string symbol = "";
+        int rest;
+
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "E";
+        }
+
+        rest = grade % 10;
+
+        if (rest >= 7 && grade < 90)
+        {
+            symbol = "+";
+        }
+        else if (rest <= 3)
+        {
+            symbol = "-";
+        }
+
+        Console.WriteLine($"The final score is {letter}{symbol}");
+
+
     }
 }
