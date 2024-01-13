7b3b33418d85f7ab6a9e860859a4f4466b3e3bb7
abimael.tavares <abimael.tavares@devsquad.com>
2024-01-13 08:44:13 -0300

    finished prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string isContinue;
+
+        do
+        {
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1, 11);
+
+            int luckNumber = randomGenerator.Next(1, 100);
+            Console.Write($"What is the magic number? {luckNumber}");
+
+            Console.WriteLine();
+
+            Console.Write("What is your guess? ");
+            int guessNumber = int.Parse(Console.ReadLine());
+
+
+            while (guessNumber != number)
+            {
+                if (number > guessNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine("Lower");
+                }
+
+                Console.WriteLine("What is your guess? ");
+                guessNumber = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("You guessed it!");
+
+            Console.WriteLine("You want to play again? yes or no");
+            isContinue = Console.ReadLine();
+
+            while (isContinue != "yes" || isContinue != "no") {
+                Console.WriteLine("Please enter a valid answer. yes or no");
+                isContinue = Console.ReadLine();
+            }
+
+        } while (isContinue == "yes");
     }
 }
