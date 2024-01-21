7c7cdf03a5d392a9319a1796f4973d5a9785cc23
Abimael Tavares <tavares@abimael.dev.br>
2024-01-21 19:16:35 -0300

    creating classes for journal challenger

---
+++ prove/Develop02/journal.cs
@@ -0,0 +1,24 @@
+public class Journal
+{
+    public List<Entry> _list = new List<Entry>();
+
+    public void AddEntry(newEntry: Entry)
+    {
+
+    }
+
+    public void DisplayAll()
+    {
+
+    }
+
+    public void SaveToFile(file string)
+    {
+
+    }
+
+    public void LoadFromFile(file string)
+    {
+
+    }
+}
