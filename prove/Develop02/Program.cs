6f2f7b37cb727f8daba44dfdffc724ea84055628
Abimael Tavares <tavares@abimael.dev.br>
2024-01-21 22:32:35 -0300

    create journal entry class

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,64 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        int choice = -1;
+        Journal journal = new Journal();
+        PromptGenerator prompt = new PromptGenerator();
+
+        while (choice < 6)
+        {
+            Console.WriteLine("Welcome to the Jornal Program!");
+            Console.WriteLine("Please select one of the following choises:");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Search");
+            Console.WriteLine("6. Quit");
+            Console.Write("What would you like to do? ");
+            choice = int.Parse(Console.ReadLine());
+
+
+            switch (choice)
+            {
+                case 1:
+                    string today = DateTime.Today.ToString("yyyy-MM-dd");
+
+                    Entry newEntry = new Entry();
+                    newEntry._promptText = prompt.GetRandomPrompt();
+                    newEntry._date = today;
+
+                    Console.WriteLine(newEntry._promptText);
+                    Console.Write("> ");
+                    newEntry._entryText = Console.ReadLine();
+                    journal.AddEntry(newEntry);
+                    break;
+
+                case 2:
+                    journal.DisplayAll();
+                    break;
+
+                case 3:
+                    Console.WriteLine("What is the filename?");
+                    string loadFileName = Console.ReadLine();
+                    journal.LoadFromFile(loadFileName);
+                    break;
+
+                case 4:
+                    Console.WriteLine("What is the filename?");
+                    string fileName = Console.ReadLine();
+                    journal.SaveToFile(fileName);
+                    break;
+
+                case 5:
+                    Console.WriteLine("Please, Enter the text to search in journal entries.");
+                    Console.Write("> ");
+                    string search = Console.ReadLine();
+                    journal.SearchAndDisplay(search);
+                    break;
+
+            }
+        }
+
     }
 }
--- prove/Develop02/promptGenerator.cs
+++ prove/Develop02/promptGenerator.cs
@@ -1,9 +1,25 @@
+using System;
+using System.Collections.Generic;
+
 public class PromptGenerator
 {
-    public List<string> _prompts;
+    public List<string> _prompts = new List<string>();
+
+    public PromptGenerator()
+    {
+        _prompts.Add("Who was the most interesting person I interacted with today?");
+        _prompts.Add("What was the best part of my day?");
+        _prompts.Add("How did I see the hand of the Lord in my life today?");
+        _prompts.Add("How did I see the hand of the Lord in my life today?");
+        _prompts.Add("How did I see the hand of the Lord in my life today?");
+    }
 
     public string GetRandomPrompt()
     {
+        Random random = new Random();
 
+        int randomIndex = random.Next(_prompts.Count);
+
+        return _prompts[randomIndex];
     }
 }
