b2d9a6e626647b7972b8bfe3d5f2f7b2890f32ed
Abimael Tavares <tavares@abimael.dev.br>
2024-01-27 21:54:21 -0300

    finished the program

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,62 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+
+        Random rnd = new Random();
+
+        int verseStart = rnd.Next(1, 36);
+
+        int verseEnd = 0;
+
+        Reference reference;
+
+        if (verseStart < 36)
+        {
+            verseEnd = verseStart + 1;
+            reference = new Reference("John", 3, verseStart, verseEnd);
+        }
+        else
+        {
+            reference = new Reference("John", 3, verseStart);
+        }
+
+
+        FileReader file = new FileReader("scripture.txt");
+
+        List<string> lines = file.GetLines();
+
+        string text = lines[verseStart + 1].Split("|")[1];
+
+        if (verseEnd != 0)
+        {
+            text += $" {lines[verseEnd + 1].Split("|")[1]}";
+        }
+
+        Scripture scripture = new Scripture(reference, text);
+
+        bool isFinished = false;
+
+        while (!isFinished)
+        {
+            Console.WriteLine(scripture.GetDisplayText());
+
+            Console.WriteLine("\nPress enter to continue or 'quit' to finish:");
+            string response = Console.ReadLine();
+
+            if (response == "quit" || scripture.AllWordsHidden())
+            {
+                isFinished = true;
+                return;
+            }
+
+
+            scripture.HideRandomWords(2);
+
+            Console.Clear();
+
+
+        }
+
+
     }
 }
